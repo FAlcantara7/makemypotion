f9e87850d17a78bf6a17891c356781ea90467846
Francisco Alcântara <francisgatesharper@gmail.com>
2024-09-03 16:22:27 -0300

    Preenchendo Pedidos.vue

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="main_container">
     <h1>PEDIDOS</h1>
+    <DashBoard/>
   </div>
 </template>
+
+<script>
+import DashBoard from '../components/Dashboard.vue'
+
+export default {
+  components: {
+    DashBoard
+  }
+}
+</script>
